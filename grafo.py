03da17aa555d3efcb82247bebb2222f53c6a437b
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-04-23 09:58:54 -0300

    Exibição

--- grafo.py
+++ grafo.py
@@ -1,3 +1,25 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 n_roteadores = 5
+
+grafo = nx.Graph()
+grafo.add_weighted_edges_from([("r1", "r2", 2), ("r2", "r4", 1), ("r1", "r3", 5), ("r1", "r4", 1)])
+
+pos = nx.spring_layout(grafo)
+
+plt.figure(figsize=(10, 5))
+
+# Subplot 1: layout padrão
+plt.subplot(121)
+nx.draw(grafo, with_labels=True, font_weight='bold')
+nx.draw_networkx_edge_labels(grafo, pos, edge_labels=nx.get_edge_attributes(grafo, 'weight'))
+
+# Subplot 2: layout shell
+plt.subplot(122)
+shell_pos = nx.shell_layout(grafo)
+nx.draw(grafo, shell_pos, with_labels=True, font_weight='bold')
+nx.draw_networkx_edge_labels(grafo, shell_pos, edge_labels=nx.get_edge_attributes(grafo, 'weight'))
+
+plt.tight_layout()
+plt.show()
---
+++ interativo.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+grafo = nx.Graph()
+grafo.add_weighted_edges_from([
+    ("r1", "r2", 2),
+    ("r2", "r4", 1),
+    ("r1", "r3", 5),
+    ("r1", "r4", 1)
+])
+
+# Dicionário com os layouts que serão testados
+layouts = {
+    "Spring Layout": nx.spring_layout,
+    "Circular Layout": nx.circular_layout,
+    "Shell Layout": nx.shell_layout,
+    "Kamada-Kawai Layout": nx.kamada_kawai_layout,
+    "Spectral Layout": nx.spectral_layout,
+    "Random Layout": nx.random_layout
+}
+
+# Criar figura com 6 subplots (2 linhas, 3 colunas)
+plt.figure(figsize=(18, 10))
+
+for i, (layout_name, layout_func) in enumerate(layouts.items(), start=1):
+    plt.subplot(2, 3, i)
+    pos = layout_func(grafo)
+    plt.title(layout_name)
+    nx.draw(grafo, pos, with_labels=True, font_weight='bold')
+    nx.draw_networkx_edge_labels(
+        grafo, pos, edge_labels=nx.get_edge_attributes(grafo, 'weight'))
+
+plt.tight_layout()
+plt.show()
