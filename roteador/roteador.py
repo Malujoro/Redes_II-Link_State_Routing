4acd958ae5f3a99e2aa9fcf173cc934a24f35677
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-04-30 14:07:16 -0300

    Organização da lógica em Classes: Roteador e HelloSender

--- roteador/roteador.py
+++ roteador/roteador.py
@@ -5,16 +5,6 @@ import threading
 import json
 import os
 
-PORTA = 5000
-BUFFER_SIZE = 4096
-
-# {
-#   "type": "HELLO",          // Tipo do pacote (poderia ser HELLO, LSA, etc.)
-#   "router_id": "R1",        // Identificador único do roteador remetente
-#   "timestamp": 1714419538,  // Momento em que o pacote foi enviado (em segundos)
-#   "interface_ip": 10.0.0.1,      // (Opcional) Interface de saída usada
-# }
-
 # {
 #   "type": "LSA",                    // Tipo do pacote
 #   "router_id": "R1",               // Roteador que criou o LSA
@@ -26,20 +16,6 @@ BUFFER_SIZE = 4096
 #   ]
 # }
 
-# Interface: (família)
-#   - 2: IPv4
-#   - 10: IPv6
-#   - 17: Endereço MAC
-
-# Função para construção do pacote Hello
-def criar_pacote_hello(router_id: str, ip_address: str):
-    return {
-        "type": "HELLO",
-        "router_id": router_id,
-        "timestamp": time.time(),
-        "ip_address": ip_address,
-    }
-
 # Função para construção do pacote LSA
 def criar_pacote_lsa(router_id: str, neighbors: list[tuple[str, int]], sequence_number: int):
     return {
@@ -49,6 +25,97 @@ def criar_pacote_lsa(router_id: str, neighbors: list[tuple[str, int]], sequence_
         "sequence_number": sequence_number,
         "links": [{"neighbor_id": neighbor_id, "cost": cost} for (neighbor_id, cost) in neighbors]
     }
+
+
+class HelloSender:
+
+    __slots__ = ["_router_id", "_interfaces",
+                 "_interval", "_PORTA"]
+
+    def __init__(self, router_id: str, interfaces: list[dict[str: str]], interval: int = 10, PORTA: int = 5000):
+        self._router_id = router_id
+        self._interfaces = interfaces
+        self._interval = interval
+        self._PORTA = PORTA
+
+    # Função para construção do pacote Hello
+    def criar_pacote(self, ip_address: str):
+        return {
+            "type": "HELLO",
+            "router_id": self._router_id,
+            "timestamp": time.time(),
+            "ip_address": ip_address,
+        }
+
+    def enviar_broadcast(self, ip_address: str, broadcast_ip: str):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        while True:
+            packet = self.criar_pacote(ip_address)
+            message = json.dumps(packet).encode('utf-8')
+
+            try:
+                sock.sendto(message, (broadcast_ip, self._PORTA))
+                print(
+                    f"[{self._router_id}:{ip_address}] Pacote HELLO enviado para {broadcast_ip}:{self._PORTA}")
+            except Exception as e:
+                print(f"[{self._router_id}:{ip_address}] Erro ao enviar: {e}")
+
+            time.sleep(self._interval)
+
+    def iniciar(self):
+        for interface_info in self._interfaces:
+            ip_address = interface_info["address"]
+            broadcast_ip = interface_info["broadcast"]
+
+            if (broadcast_ip != None):
+                thread_emissor = threading.Thread(target=self.enviar_broadcast, args=(
+                    ip_address, broadcast_ip), daemon=True)
+                thread_emissor.start()
+
+class Roteador:
+
+    __slots__ = ["_router_id", "_interfaces", "_PORTA", "_BUFFER_SIZE"]
+
+    def __init__(self, router_id: str, interfaces: list[dict[str: str]], PORTA: int = 5000, BUFFER_SIZE: int = 4096):
+        self._router_id = router_id
+        self._interfaces = interfaces
+        self._PORTA = PORTA
+        self._BUFFER_SIZE = BUFFER_SIZE
+
+    def receber_pacotes(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Escuta em todas as interfaces
+        sock.bind(('', self._PORTA))
+
+        print(f"Receptor HELLO escutando na porta {self._PORTA}...")
+
+        while True:
+            try:
+                data, addr = sock.recvfrom(self._BUFFER_SIZE)
+                mensagem = data.decode('utf-8')
+                pacote = json.loads(mensagem)
+                received_id = pacote.get("router_id")
+                if (pacote.get("type") == "HELLO" and received_id != self._router_id):
+                    print(
+                        f"\nPacote HELLO recebido de {received_id}:{addr[0]}")
+                    print(json.dumps(pacote, indent=4))
+            except Exception as e:
+                print(f"Erro ao receber pacote: {e}")
+
+    def iniciar(self):
+        hello = HelloSender(self._router_id, self._interfaces)
+        # self._lsa = LSA(router_id, interfaces)
+
+        thread_receptor = threading.Thread(
+            target=self.receber_pacotes, daemon=True)
+        thread_receptor.start()
+
+        hello.iniciar()
+
+        while True:
+            time.sleep(1)
 
 
 def listar_enderecos():
@@ -65,60 +132,13 @@ def listar_enderecos():
     return interfaces_list
 
 
-def enviar_hello_broadcast(router_id: str, ip_address: str, broadcast_ip: str, interval: int = 10):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-    while True:
-        packet = criar_pacote_hello(router_id, ip_address)
-        message = json.dumps(packet).encode('utf-8')
-
-        try:
-            sock.sendto(message, (broadcast_ip, PORTA))
-            print(
-                f"[{router_id}:{ip_address}] Pacote HELLO enviado para {broadcast_ip}:{PORTA}")
-        except Exception as e:
-            print(f"[{router_id}:{ip_address}] Erro ao enviar: {e}")
-
-        time.sleep(interval)
-
-def receber_hello(router_id: str):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Escuta em todas as interfaces
-    sock.bind(('', PORTA))
-
-    print(f"Receptor HELLO escutando na porta {PORTA}...")
-
-    while True:
-        try:
-            data, addr = sock.recvfrom(BUFFER_SIZE)
-            mensagem = data.decode('utf-8')
-            pacote = json.loads(mensagem)
-            received_id = pacote.get("router_id")
-            if (pacote.get("type") == "HELLO" and received_id != router_id):
-                print(f"\nPacote HELLO recebido de {received_id}:{addr[0]}")
-                print(json.dumps(pacote, indent=4))
-        except Exception as e:
-            print(f"Erro ao receber pacote: {e}")
-
-
 if (__name__ == "__main__"):
     router_id = os.getenv('CONTAINER_NAME')
+    if (not router_id):
+        raise ValueError(
+            "CONTAINER_NAME não definido nas variáveis de ambiente")
+
     interfaces = listar_enderecos()
 
-    # Iniciar receptor em thread
-    thread_receptor = threading.Thread(
-        target=receber_hello, args=(router_id,), daemon=True)
-    thread_receptor.start()
-
-    for dicio in interfaces:
-        ip_address = dicio["address"]
-        broadcast_ip = dicio["broadcast"]
-
-        if (broadcast_ip != None):
-            thread = threading.Thread(target=enviar_hello_broadcast, args=(
-                router_id, ip_address, broadcast_ip), daemon=True)
-            thread.start()
-
-    while True:
-        time.sleep(1)
+    roteador = Roteador(router_id, interfaces)
+    roteador.iniciar()
